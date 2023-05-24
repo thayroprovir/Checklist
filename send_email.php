414bf5a874db69dd3778d80099267bf4717922a7
thayroprovir <134451159+thayroprovir@users.noreply.github.com>
2023-05-24 12:11:52 -0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,51 +1,71 @@
 <?php
-// Recupera os dados do formulário
-$empresa = $_POST['empresa'];
-$ambiente = $_POST['ambiente'];
-
-// Recupera as informações das funções e riscos adicionados dinamicamente
-$funcoes = $_POST['funcao'];
-$cbos = $_POST['cbo'];
-$riscoNomes = $_POST['nomeRisco'];
-$fontesRisco = $_POST['fonteRisco'];
-$exposicoesRisco = $_POST['exposicaoRisco'];
-$propagacoesRisco = $_POST['propagacaoRisco'];
-$lesoesRisco = $_POST['lesoesRisco'];
-$tiposAvaliacaoRisco = $_POST['tipoAvaliacaoRisco'];
-
-// Monta o conteúdo do e-mail
-$subject = "Checklist Provir Segurança Ocupacional - Dados";
-$message = "Empresa: $empresa\n";
-$message .= "Ambiente: $ambiente\n\n";
-
-// Itera sobre as funções e riscos para adicionar ao conteúdo do e-mail
-foreach ($funcoes as $i => $funcao) {
-    $message .= "Função ".($i + 1).": $funcao\n";
-    $message .= "CBO: $cbos[$i]\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Dados do formulário
+    $empresa = $_POST["empresa"];
+    $ambiente = $_POST["ambiente"];
     
-    // Itera sobre os riscos para adicionar ao conteúdo do e-mail
-    foreach ($riscoNomes[$i] as $j => $nomeRisco) {
-        $message .= "Risco ".($j + 1).": $nomeRisco\n";
-        $message .= "Fonte do Risco: $fontesRisco[$i][$j]\n";
-        $message .= "Exposição do Risco: $exposicoesRisco[$i][$j]\n";
-        $message .= "Meio de Propagação: $propagacoesRisco[$i][$j]\n";
-        $message .= "Lesões: $lesoesRisco[$i][$j]\n";
-        $message .= "Tipo de Avaliação: $tiposAvaliacaoRisco[$i][$j]\n\n";
+    // Processar funções e riscos
+    $funcoes = array();
+    if (isset($_POST["funcao"])) {
+        $funcaoCount = count($_POST["funcao"]);
+        
+        for ($i = 0; $i < $funcaoCount; $i++) {
+            $funcao = $_POST["funcao"][$i];
+            $riscos = array();
+            
+            if (isset($_POST["nomeRisco"][$i])) {
+                $riscoCount = count($_POST["nomeRisco"][$i]);
+                
+                for ($j = 0; $j < $riscoCount; $j++) {
+                    $nomeRisco = $_POST["nomeRisco"][$i][$j];
+                    $fonteRisco = $_POST["fonteRisco"][$i][$j];
+                    $exposicaoRisco = $_POST["exposicaoRisco"][$i][$j];
+                    $propagacaoRisco = $_POST["propagacaoRisco"][$i][$j];
+                    $lesaoRisco = $_POST["lesaoRisco"][$i][$j];
+                    $tipoAvaliacaoRisco = $_POST["tipoAvaliacaoRisco"][$i][$j];
+                    
+                    $risco = array(
+                        "nome" => $nomeRisco,
+                        "fonte" => $fonteRisco,
+                        "exposicao" => $exposicaoRisco,
+                        "propagacao" => $propagacaoRisco,
+                        "lesao" => $lesaoRisco,
+                        "tipoAvaliacao" => $tipoAvaliacaoRisco
+                    );
+                    
+                    if ($tipoAvaliacaoRisco == "quantitativo" && isset($_FILES["avaliacaoQuantitativa"][$i][$j])) {
+                        // Processar o arquivo de avaliação quantitativa, se fornecido
+                        $avaliacaoQuantitativa = $_FILES["avaliacaoQuantitativa"][$i][$j];
+                        // Faça o processamento necessário do arquivo
+                        // Por exemplo, mover o arquivo para um diretório específico e armazenar o caminho em $risco
+                        
+                        // Exemplo de movimentação do arquivo para um diretório (diretório deve existir)
+                        $targetDirectory = "Ccaminha\anexo\";
+                        $targetFilename = $targetDirectory . basename($avaliacaoQuantitativa["name"]);
+                        
+                        if (move_uploaded_file($avaliacaoQuantitativa["tmp_name"], $targetFilename)) {
+                            $risco["avaliacaoQuantitativa"] = $targetFilename;
+                        } else {
+                            // Falha no upload do arquivo
+                            // Lide com o erro de acordo com os requisitos do seu aplicativo
+                        }
+                    }
+                    
+                    $riscos[] = $risco;
+                }
+            }
+            
+            $funcaoItem = array(
+                "funcao" => $funcao,
+                "riscos" => $riscos
+            );
+            
+            $funcoes[] = $funcaoItem;
+        }
     }
-    $message .= "\n";
-}
-
-// Configurações do e-mail
-$to = "administracao@clinicaporvir.com";
-$headers = "From: checklist@clinicaporvir.com";
-
-// Envia o e-mail
-mail($to, $subject, $message, $headers);
-
-// Criação do arquivo Excel
-// Aqui você pode usar uma biblioteca como PHPExcel ou PHPSpreadsheet para gerar o arquivo Excel com os dados
-
-// Redireciona de volta para a página principal
-header("Location: index.html");
-exit();
-?>
+    
+    // Processar os dados como necessário
+    
+    // Exemplo de exibição dos dados recebidos
+    echo "Empresa: " . $empresa . "<br>";
+    echo "Ambiente.
